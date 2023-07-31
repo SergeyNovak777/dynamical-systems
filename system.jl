cf610db074d610a42ac69a91856332ad446a7c4c
SergeyNovak777 <sergeyheart@mail.ru>
2023-07-31 21:26:06 +0300

    ....

--- system.jl
+++ system.jl
@@ -11,6 +11,19 @@
     return SVector(du1, du2, du3)
 end
 
+@inbounds function TM_bk(u, p)
+    U(y, p) = p[8] + p[9] / ( 1.0 + exp( -50.0 * (y - p[7]) ) )
+    σ(x, p) = 1.0 / ( 1.0 + exp( -20.0 * (x-p[6]) ) )
+    g(E, x, y, p, U_) = log( 1.0 + exp( (p[5] * U_ * x * E + p[11]  ) / (p[1]) ) )
+    
+    U_ = U(u[3], p)
+    du1 = (-u[1] + p[1] * g(u[1], u[2], u[3], p, U_) ) / p[2]
+    du2 = (1.0 - u[2]) / p[3] - U_*u[2]*u[1]
+    du3 = (-u[3])/p[4] + p[10] * σ(u[2], p)
+    
+    return [du1, du2, du3]
+end
+
 @inbounds function jacob_TM_(u, p, t)
     
     U(y, p, exp50) = p[8] + p[9] / ( 1.0 + exp50 )
