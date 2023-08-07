7fa76e3208ca81b4ee492d8821260f4ea9d72b12
SergeyNovak777 <sergeyheart@mail.ru>
2023-08-07 19:37:02 +0300

    Fold curve

--- Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
+++ Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
@@ -2,15 +2,15 @@ using Pkg
 Pkg.activate("C:\\Users\\Alex\\Desktop\\dynamical-systems\\env\\bifurcation\\")
 cd("C:\\Users\\Alex\\Desktop\\dynamical-systems\\Tsodyks Markram\\Levanova\\3 набор параметров\\Сопоставление с матконт\\файлы matlab")
 using MAT, JLD
-pathx= "I0_hom_stump.mat"
-pathy= "U0_hom_stump.mat"
+pathx= "I0_Fold.mat"
+pathy= "U0_Fold.mat"
 
 file = matopen(pathx)
-I0_ = read(file, "I0_hom_stump")
+I0_ = read(file, "I0_Fold")
 close(file)
 
 file = matopen(pathy)
-U0_ = read(file, "U0_hom_stump")
+U0_ = read(file, "U0_Fold")
 close(file)
 
-save("I0_hom_stump.jld", "data",I0_); save("U0_hom_stump.jld", "data",U0_)
+save("I0_Fold.jld", "data",I0_); save("U0_Fold.jld", "data",U0_)
