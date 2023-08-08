e2df838fc0a3e65ca7abc577081bd22d7b709439
SergeyNovak777 <sergeyheart@mail.ru>
2023-08-08 16:56:52 +0300

    ...

--- Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
+++ Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
@@ -2,15 +2,15 @@ using Pkg
 Pkg.activate("C:\\Users\\Alex\\Desktop\\dynamical-systems\\env\\bifurcation\\")
 cd("C:\\Users\\Alex\\Desktop\\dynamical-systems\\Tsodyks Markram\\Levanova\\3 набор параметров\\Сопоставление с матконт\\файлы matlab")
 using MAT, JLD
-pathx= "I0_Fold.mat"
-pathy= "U0_Fold.mat"
+pathx= "I0_hom_bt.mat"
+pathy= "U0_hom_bt.mat"
 
 file = matopen(pathx)
-I0_ = read(file, "I0_Fold")
+I0_ = read(file, "I0_hom_bt")
 close(file)
 
 file = matopen(pathy)
-U0_ = read(file, "U0_Fold")
+U0_ = read(file, "U0_hom_bt")
 close(file)
 
-save("I0_Fold.jld", "data",I0_); save("U0_Fold.jld", "data",U0_)
+save("I0_hom_bt.jld", "data",I0_); save("U0_hom_bt.jld", "data",U0_)
