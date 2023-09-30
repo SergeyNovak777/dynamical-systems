4654c85f24949500788bbc97c78457b88ca2bfb5
SergeyNovak777 <sergeyheart@mail.ru>
2023-09-30 19:26:00 +0300

    First lyapunov coeff AH

--- Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
+++ Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
@@ -2,15 +2,15 @@ using Pkg
 Pkg.activate("C:\\Users\\Alex\\Desktop\\dynamical-systems\\env\\bifurcation\\")
 cd("C:\\Users\\Alex\\Desktop\\dynamical-systems\\Tsodyks Markram\\Levanova\\3 набор параметров\\Сопоставление с матконт\\файлы matlab")
 using MAT, JLD
-pathx= "vars.mat"
+pathx= "CLAH.mat"
 #pathy= "U0_hom_bt.mat"
 
 file = matopen(pathx)
-I0_ = read(file, "vars")
+I0_ = read(file, "CLAH")
 close(file)
 
 #file = matopen(pathy)
 #U0_ = read(file, "U0_hom_bt")
 #close(file)
 
-save("vars.jld", "data",I0_); #save("U0_hom_bt.jld", "data",U0_)
+save("CLAH.jld", "data",I0_); #save("U0_hom_bt.jld", "data",U0_)
