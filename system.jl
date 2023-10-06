4ef6ff0bb5afee8f1b3c953b3b863e3cdc9c7746
nova ubuntu <sergeyheart@mail.ru>
2023-10-06 13:23:45 +0300

    added TM6

--- system.jl
+++ system.jl
@@ -207,23 +207,95 @@ end
 # ---------------------------------------------------------------------------------
 # model tetrapartite synapse
 
-function tetrapart_model(var, par, t)
+@inbounds function TM6_glial_ECM(var, par, t)
 
-    E, x, u, y, ecm, p  = var;
-    τ, τD, τF, τy, α, αE, αecm, αp, J, U0, I0, ΔU0, β, βecm, βp, γp, ecm0, ecm1, kecm, θecm, p0, p1, θp, kp, ythr, xthr = par;
+    g = log( 1.0 + exp( ( (par[9] + par[6] * var[5]) * var[3] * var[2] * var[1] + par[11]) / par[5] ) );
+    U = par[10] + par[12] / ( 1.0 + exp( -50.0 * ( var[4] - par[25] ) ) );
+    Hy = 1.0 / ( 1.0 + exp( -20.0 * ( var[2] - par[26] ) ) );
+    Hecm = par[17] - (par[17] - par[18]) / (1.0 + exp( -(var[1] - par[20]) / par[19] ) );
+    Hp =  par[21] - (par[21] - par[22]) / (1.0 + exp( -(var[1] - par[23]) / par[24]) );
 
-    g = log( 1 + exp( ( (J + αE * ecm) * u * x * E + I0) / α ) );
-    U = U0 + ΔU0 / ( 1 + exp( -50 * ( y - ythr ) ) );
-    Hy = 1 / ( 1 + exp( -20 * ( x - xthr ) ) );
-    Hecm = ecm0 - (ecm0 - ecm1) / (1 + exp( -(E - θecm) / kecm ) );
-    Hp =  p0 - (p0 - p1) / (1 + exp( -(E - θp) / kp) );
-
-    dE = (-E + α * g) / τ;
-    dx = (1 - x) / τD  -u * x * E;
-    du = (U - u) / τF  + U * (1 - u) * E;
-    dy = (-y) / τy + β * Hy;
-    decm = -( αecm + γp * p ) * ecm + βecm * Hecm;
-    dp = -αp * p + βp * Hp;
+    dE = (-var[1] + par[5] * g) / par[1];
+    dx = (1.0 - var[2]) / par[2]  -var[3] * var[2] * var[1];
+    du = (U - var[3]) / par[3]  + U * (1.0 - var[3]) * var[1];
+    dy = (-var[4]) / par[4] + par[13] * Hy;
+    decm = -( par[7] + par[16] * var[6] ) * var[5] + par[14] * Hecm; 
+    dp = -par[8] * var[6] + par[15] * Hp;
 
     return SVector(dE, dx, du, dy, decm, dp);
+end
+
+@inbounds function TM6_glial_ECM_jac(vars, params, t)
+
+    E_sum =  params[9] + params[6] * vars[5];
+
+    exp_ = exp( ( E_sum * vars[3] * vars[2] * vars[1] + params[11] ) / params[5] );
+    
+    denomE = 1.0 + exp_;
+
+    exp50 = exp( -50.0 * ( vars[4] - params[25] ) );
+    exp20 = exp( -20.0 * ( vars[2] - params[26] ) );
+    exphecm = exp( -(vars[1] - params[20]) / (params[19]) );
+    exphp = exp( -(vars[1] - params[23]) / (params[24]) );
+
+    U = params[10] + params[12] / ( 1.0 + exp( -50.0 * ( vars[4] - params[25] ) ) );
+    Uder = (50.0 * params[12] * exp50) / ( 1.0 + exp50 )^2;
+
+    Hyder = 20.0 * exp20 / (1.0 + exp20)^2;
+
+    Hecmder = (params[17] - params[18]) * exphecm / ( params[19] * (1.0 + exphecm)^2 );
+    Hpder = (params[21] - params[22]) * exphp / ( params[24] * (1.0 + exphp)^2 );
+
+    EE = ( -1.0 + ( E_sum * vars[3] * vars[2] * exp_ ) / denomE ) / params[1];
+    Ex = ( ( E_sum * vars[3] * vars[1] * exp_ ) / denomE ) / params[1];
+    Eu = ( ( E_sum * vars[2] * vars[1] * exp_ ) / denomE ) / params[1];
+    Eecm = ( (exp_ * params[6] * vars[3] * vars[2] * vars[1]) / denomE ) / params[1];
+
+    xE = -vars[3] * vars[2];
+    xx = - 1.0 / params[2] - vars[3] * vars[1];
+    xu = -vars[2] * vars[1];
+
+
+    uE = U * (1.0 - vars[3]);
+    uu = -1.0 / params[3] - U * vars[1];
+    uy = Uder * ( 1.0 / params[3] + (1.0 - vars[3]) * vars[1] );
+
+    yx = params[13] * Hyder;
+
+    ecmE =  -params[14] * Hecmder;
+    ecmecm = -(params[7] + params[16] * vars[6]);
+    ecmp = -vars[5] * params[16];
+
+    pE = -params[15] * Hpder;
+
+    SMatrix{6,6}(EE, xE, uE, 0.0, ecmE, pE,
+        Ex, xx, 0.0, yx, 0.0, 0.0,
+        Eu, xu, uu, 0.0, 0.0, 0.0,
+        0.0, 0.0, uy, - 1.0 / params[4], 0.0, 0.0,
+        Eecm, 0.0, 0.0, 0.0, ecmecm, 0.0,
+        0.0, 0.0, 0.0, 0.0, ecmp, -params[8])
+end
+
+function TM6_glial_ECM_get_params()
+    τ = 0.013; τD = 0.15; τF = 1.0; τy = 1.8;   
+    α = 1.5; αecm = 0.001; αp = 0.01;
+    J = 3.07; U0 = 0.3; ΔU0 = 0.305; 
+    β = 0.438; βp = 0.01; βecm = 0.01;
+    ecm0 = 0.0; ecm1 = 1.0; kecm = 0.15; θecm = 25.6;
+    p0 = 0.0; p1 = 1.0; kp = 0.05; γp = 0.1; θp = 26.0; 
+    ythr = 0.5; xthr = 0.9;
+    αE = 5.0;
+    I0 = -1.741;
+
+    param = [τ, τD, τF, τy, α, αE, αecm, αp, J, U0, I0, ΔU0, β, βecm, βp, γp, ecm0, ecm1, kecm, θecm, p0, p1, θp, kp, ythr, xthr];
+    return param;
+end
+function TM6_glial_ECM_help()
+    params = "
+    τ - 1, τD - 2, τF - 3, τy - 4, α - 5, αE - 6, αecm - 7, αp - 8,
+    J - 9, U0 - 10, I0 - 11, ΔU0 - 12, β - 13, βecm  -14, βp - 15,
+    γp - 16, ecm0 - 17, ecm1 - 18, kecm - 19, θecm - 20, p0 - 21, p1 - 22, θp - 23, kp - 24, ythr - 25, xthr - 26";
+    vars = "E - 1, x - 2, u - 3, y - 4, ECM - 5, P - 6";
+
+    return vars, params
 end
