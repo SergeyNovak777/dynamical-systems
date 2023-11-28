3fbe3022f4e16195d31d3202c7f29f555b197995
SergeyNovak777 <sergeyheart@mail.ru>
2023-11-28 17:20:24 +0300

    matcont files

--- Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
+++ Tsodyks Markram/Levanova/3 набор параметров/Сопоставление с матконт/convert to jl.jl
@@ -2,15 +2,15 @@ using Pkg
 Pkg.activate("C:\\Users\\Alex\\Desktop\\dynamical-systems\\env\\bifurcation\\")
 cd("C:\\Users\\Alex\\Desktop\\dynamical-systems\\Tsodyks Markram\\Levanova\\3 набор параметров\\Сопоставление с матконт\\файлы matlab")
 using MAT, JLD
-pathx= "CLAH.mat"
+pathx= "x_to_top_righter.mat"
 #pathy= "U0_hom_bt.mat"
 
 file = matopen(pathx)
-I0_ = read(file, "CLAH")
+output = read(file, "x")
 close(file)
 
 #file = matopen(pathy)
 #U0_ = read(file, "U0_hom_bt")
 #close(file)
 
-save("CLAH.jld", "data",I0_); #save("U0_hom_bt.jld", "data",U0_)
+save("x_to_top_righter.jld", "data",output); #save("U0_hom_bt.jld", "data",U0_)
