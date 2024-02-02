551435ca026f377643ce1428326db6a436aeb4fd
SergeyNovak777 <sergeyheart@mail.ru>
2024-02-02 16:53:45 +0300

    ...

--- rate model/modes_from_article.jl
+++ rate model/modes_from_article.jl
@@ -1,1 +1,16 @@
-include("C:\\Users\\Alex\\Desktop\\dynamical-systems\\system.jl");
+if Sys.iswindows()
+    username = "Alex"
+    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
+    using Pkg
+    Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
+    using DifferentialEquations, DynamicalSystems, CairoMakie
+    include("C:\\Users\\Alex\\Desktop\\dynamical-systems\\system.jl");
+else
+    username = "nova"
+    pathtorepo = "/home/nova/work/repo_ds/dynamical-systems"
+    using Pkg
+    Pkg.activate(pathtorepo * "/env/integrate/")
+    using DifferentialEquations, DynamicalSystems, CairoMakie
+end
+
+integrate_setting = (alg = RK4(), adaptive = false, dt = 0.01)
