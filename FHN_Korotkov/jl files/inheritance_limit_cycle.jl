525dd4353d42c639ae70501bb3b3600247e5e45c
SergeyNovak777 <sergeyheart@mail.ru>
2024-03-06 11:17:45 +0300

    reorganization FHN2 Korotkov

---
+++ FHN_Korotkov/jl files/inheritance_limit_cycle.jl
@@ -0,0 +1,116 @@
+if Sys.iswindows()
+    username = "Alex"
+    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
+    using Pkg
+    Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
+else
+    username = "sergey"
+    pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
+    using Pkg
+    Pkg.activate(pathtorepo * "/env/integrate/")
+end
+
+include("/home/sergey/work/repo/dynamical-systems/system.jl")
+
+using DifferentialEquations, DynamicalSystems, StaticArrays, JLD2, GLMakie
+
+#=function plot_phase_space(sol, tstart, tend, indexs, labels)
+    indexx, indexy, indexz = indexs
+    labelx, labely, labelz = labels
+    f = Figure(size = (400, 400))
+    ax = Axis3(f[1,1], xlabel = labelx, ylabel = labely, zlabel = labelz)
+    lines!(ax, sol[indexx, tstart:tend], sol[indexy, tstart:tend], sol[indexz, tstart:tend])
+    display(GLMakie.Screen(), f)
+end=#
+
+function goto_attractor(u0_loc, parameters, tspan)
+    prob = ODEProblem(FHN2_try3, u0_loc, tspan, parameters)
+    sol = solve(prob, RK4(), adaptive = false, dt = 0.001, maxiters = 5e6,
+    save_everystep = false, save_start = false);
+    last_point = sol[end]
+    return last_point
+end
+
+function save_files(filenameu0s, filenameLSEs)
+    jldsave(filenameu0s; u0ss)
+    jldsave(filenameLSEs; LSEs)
+end
+
+function calculate_LSE(u0_lc, params, time_calculate_LLE)
+    ds = CoupledODEs(FHN2_try3, u0_lc, params,
+     diffeq = ( alg = RK4(), adaptive = false, dt = 0.001, maxiters = 5e6 ));
+    LLE = lyapunovspectrum(ds, time_calculate_LLE)
+    return LLE
+end
+
+function print_output(index, parameter, u0, point, LSE)
+    println("index: $(index); k_1: $(parameter)"); flush(stdout)
+    println("u0: $(u0)"); flush(stdout)
+    println("last point: $(point)");flush(stdout)
+    println("LSE: $(LSE)"); flush(stdout)
+    println("-----------------------------"); flush(stdout)
+    println("");flush(stdout)
+end
+
+function inheritance_limit_cycle(index_p, range_param, tspan, time_calculate_LLE)
+
+    u0 = SVector{5}([-1.0, 0.0, 0.01, -1.0, 0.0])
+    parameters = FHN2_try3_params()
+    
+    for index in range(1,length(range_param))
+
+        if index == 1
+            global u0_local = u0
+        end
+
+        parameters[index_p] = range_param[index]
+        point = goto_attractor(u0_local, parameters, tspan)
+        LSE = calculate_LSE(point, parameters, time_calculate_LLE)
+
+        print_output(index, parameters[index_p], u0_local, point, LSE)
+
+        u0_local = point
+
+        u0ss[index, :] = point
+        LSEs[index, :] = LSE
+
+
+        if mod(index, 10) == 0
+            save_files(filenameu0s, filenameLSEs)
+        end
+    end
+end
+
+cd("/home/sergey/work/repo/dynamical-systems/FHN_Korotkov/data")
+
+tspan = (0.0, 500)
+time_calculate_LLE = 5000
+
+k1_start = 0.0
+k1_end = 0.094
+len = 1000
+index_param = 7
+range_param = range(k1_start, k1_end, length = len)
+
+u0ss = zeros(len, 5)
+LSEs = zeros(len, 5)
+
+filenameu0s = "u0s_k1_length_1000.jld2"
+filenameLSEs = "LSEs_k1_length_1000.jld2"
+
+inheritance_limit_cycle(index_param, range_param, tspan, time_calculate_LLE)
+
+#=parameters = FHN2_try3_params()
+parameters[7] = rangek1[1]
+
+prob = ODEProblem(FHN2_try3, point, tspan, parameters)
+sol = solve(prob, RK4(), adaptive = false, dt = 0.001, maxiters = 5e6)
+
+tstart = round(Int, length(sol) / 2); tend = length(sol)
+indexx, indexy, indexz = 1, 3, 4
+indexs = [indexx, indexy, indexz]
+labelx, labely, labelz = "x1", "x2", "y2"
+labels = [labelx, labely, labelz]
+
+plot_phase_space(sol, tstart, tend, indexs, labels)=#
+
---
+++ FHN_Korotkov/jl files/poincare_diagram k2.jl
@@ -0,0 +1,96 @@
+if Sys.iswindows()
+    username = "Alex"
+    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
+    using Pkg
+    Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
+else
+    username = "sergey"
+    pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
+    using Pkg
+    Pkg.activate(pathtorepo * "/env/integrate/")
+end
+
+using DifferentialEquations, DynamicalSystems, StaticArrays, GLMakie, JLD2
+
+function FHN2_try3(u, p ,t)
+    x1, y1, x2, y2, z= u
+    ϵ, a, g, k, σ, α, k1, k2 = p
+
+    I(ϕ_i) = g * (1.0/(1.0 + exp(k*(cos(σ/2) - cos(ϕ_i - α - σ/2)))))
+    ρz = k1 + k2 * z ^ 2
+
+    ϕ2 = atan(y2, x2)
+    ϕ1 = atan(y1, x1)
+
+    dx1dt = (x1 - x1 ^ 3 / 3 - y1 + I(ϕ2) + ρz * (x2 - x1) ) / ϵ
+    dy1dt = x1 - a
+    dx2dt = (x2 - x2 ^ 3 / 3 - y2 + I(ϕ1) + ρz * (x1 - x2) ) / ϵ
+    dy2dt = x2 - a
+    dzdt = x1 - x2
+    return SVector(dx1dt, dy1dt, dx2dt, dy2dt, dzdt)
+end
+
+function FHN2_try3_params()
+    ϵ = 0.01; a = -1.01;
+    g = 0.1; k = 50.0; σ = 50.0 * pi / 180; α = 160.0 * pi / 180;
+    k1 = 0.0; k2 = 0.0
+    return [ ϵ, a, g, k, σ, α, k1, k2]
+end
+
+function plot_bifurcation_diagram(output, rangek1)
+    markersize = 0.5;
+    lbsize = 30;
+    ticksize = 30;
+    fig = Figure(size = (1200, 350))
+    axis = Axis(fig[1,1],
+    xlabel = L"k_2",  ylabel = L"x_1",
+    xlabelsize = lbsize, ylabelsize = lbsize,
+    xticklabelsize = ticksize,yticklabelsize = ticksize)
+    
+    for (j, p) in enumerate(rangek1)
+        scatter!(axis, fill(p, length(output[j])), output[j];
+         color = ("black", 0.5), markersize = markersize)
+    end
+    display(GLMakie.Screen(), fig)
+end
+function bifurcation_diagram()
+
+    u0 = [-1.0099720067534301, -0.6379550713256545, -1.0103795313749988, -0.635237400779188, -0.007823015750248142]
+    params = FHN2_try3_params()
+    params[7] = 0.09
+    integ_set = (alg = RK4(), adaptive = false, dt = 0.001)
+
+    ds = CoupledODEs(FHN2_try3, u0, params, diffeq = integ_set)
+   
+    t = 2000
+    ttr = 500
+
+    k2_start = 0.0
+    k2_end = 100.0
+    len = 2000
+    rangek2 = range(k2_start, k2_end, length = len)
+    index_control_param = 8
+
+    index_saving_var = 1
+    index_fixed_var = 3
+    value_fixed_var = -1.01
+    surface = (index_fixed_var, value_fixed_var)
+
+    pmap = PoincareMap(ds, surface, direction = 1)# Tmax = 1e6)
+    
+    output = orbitdiagram(pmap, index_saving_var, index_control_param, rangek2;
+     n = t, Ttr = ttr, show_progress = true)
+
+     return output
+end
+
+k2_start = 0.0
+k2_end = 100.0
+len = 2000
+rangek2 = range(k2_start, k2_end, length = len)
+
+output = bifurcation_diagram()
+
+plot_bifurcation_diagram(output, rangek2)
+
+jldsave("bif_dia_k2_length_2000_2000_secant_x2_-101_direction_1.jld2";output)
