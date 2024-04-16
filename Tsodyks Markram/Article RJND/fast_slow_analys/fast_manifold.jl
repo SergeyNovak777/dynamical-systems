b457f2bb3a79e72c6b6156657635aefa81fb6c26
SergeyNovak777 <sergeyheart@mail.ru>
2024-04-16 17:06:29 +0300

    implicit plot fast manifold

---
+++ Tsodyks Markram/Article RJND/fast_slow_analys/fast_manifold.jl
@@ -0,0 +1,17 @@
+if Sys.iswindows()
+    username = "Alex"
+    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
+    using Pkg
+    Pkg.activate(pathtorepo * "dynamical-systems\\env\\implicit\\")
+else
+    username = "sergey"
+    pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
+    using Pkg
+    Pkg.activate(pathtorepo * "/env/implicit/")
+end
+
+using Implicit3DPlotting
+
+f(x) = x[3] - 1.58 * log( 1 + exp( ( 3.07 * ( 0.265 + 0.305 / ( 1 + exp( -50 * ( x[2]  -0.4 ) ) ) ) * x[1] * x[3] -1.6  ) / 1.58 ) )
+
+scene = plot_implicit_surface(f; transparency=false, xlims=(0,1), ylims=(0,1), zlims=(0,30))
