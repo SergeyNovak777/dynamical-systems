92c73b39f2ca71d21980740dd501a5ed7aa4f28a
Sergey_Olenin <sergeyheart@mail.ru>
2024-05-06 14:04:26 +0300

    pdf

--- FHN_Korotkov/PDF/mode_k2=75.74.jl
+++ FHN_Korotkov/PDF/mode_k2=75.74.jl
@@ -20,41 +20,40 @@ parameters = FHN2_try3_params()
 tspan = (0.0, 50000.0)
 parameters[7] = 0.09
 parameters[8] = 75.74
-
 u0 = [1.7, 0.7, -1.4, 0.35, 0.7 - 0.35]
 u0 = SVector{5}(u0)
 prob = ODEProblem(FHN2_try3, u0, tspan, parameters)
 sol = solve(prob, DP8(), adaptive = true, abstol = 1e-11, reltol = 1e-11, maxiters = 1e8);
 len_sol = length(sol.t)
-
 tstart = t_truncate(len_sol); tend = len_sol
 
 data = [sol[1, tstart:tend], sol.t[tstart:tend]]
-array_peaks, array_t_peaks = get_peaks_neg(data)
-Hs_x = Hs_below(array_peaks, 4)
-σ = Statistics.mean(array_peaks)
+array_peaks, array_t_peaks = get_peaks(data; level_zero = "nothing")
+# get_peaks_neg(data)
+array_peaks_abs = abs.(array_peaks)
+Hs_x = Hs_above(array_peaks_abs, 8)
+σ = Statistics.mean(array_peaks_abs)
 
 width_window = 500; height_window = 1000;
 f = Figure(size = (height_window, width_window))
 ax = Axis(f[1, 1], xlabel = L"time", ylabel =  L"x")
-lines!(ax, sol.t[2720000:tend], sol[1, 2720000:tend], color = :blue, linewidth = 0.5)
+lines!(ax, sol.t[2600000:tend], sol[1, 2600000:tend], color = :blue, linewidth = 0.5)
 scatter!(ax, array_t_peaks, array_peaks, markersize = 10, color = :red)
-xlims!(ax, 49830, 49930)
+xlims!(ax, 49700, 49930)
 hlines!(Hs_x, color = "red", linewidth = 2.0, linestyle = :dash)
-#hlines!(σ , color = "lime", linewidth = 2.0, linestyle = :dash)
+hlines!(σ , color = "lime", linewidth = 2.0, linestyle = :dash)
 display(GLMakie.Screen(), f)    
 
 count_thesholds = 1000000
-maxvalue = minimum(array_peaks) # minimum(sol[1, tstart:tend])
-maxvalue = abs(minvalue)
-minvalue = abs(maximum(array_peaks))
-ϵ = 0.9
-threshold_range, array_PDF = pdf_v2(abs.(array_peaks), count_thesholds, minvalue, maxvalue, ϵ)
+maxvalue = maximum(array_peaks_abs) # minimum(sol[1, tstart:tend])
+minvalue = minimum(array_peaks_abs)
+ϵ = 1.5
+threshold_range, array_PDF = pdf_v2(array_peaks_abs, count_thesholds, minvalue, maxvalue, ϵ)
 
 f = Figure(size = (400, 400))
 ax = Axis(f[1, 1])
-lines!(threshold_range, array_PDF, linewidth = 0.5)
-vlines!(ax, abs.(Hs_x), color = "red", linestyle = :dash, linewidth = 1.0)
+lines!(threshold_range, array_PDF, linewidth = 1.5)
+vlines!(ax, Hs_x, color = "red", linestyle = :dash, linewidth = 1.5)
 display(GLMakie.Screen(), f)
 
 
