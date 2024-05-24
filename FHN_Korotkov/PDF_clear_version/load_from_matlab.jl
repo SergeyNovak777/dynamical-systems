22b2012b3989d77661ae27012e56bce919ee09ed
Sergey Olenin <sergeyheart@mail.ru>
2024-05-24 17:57:02 +0300

    FHN PDF IEI
    update load mat files

--- FHN_Korotkov/PDF_clear_version/load_from_matlab.jl
+++ FHN_Korotkov/PDF_clear_version/load_from_matlab.jl
@@ -10,7 +10,7 @@ else
     Pkg.activate(pathtorepo * "/env/integrate/")
 end
 
-using Statistics, MAT, CairoMakie, GLMakie
+using Statistics, MAT, JLD2, CairoMakie, GLMakie
 
 include("/home/sergey/work/repo/dynamical-systems/system.jl")
 include("/home/sergey/work/repo/dynamical-systems/FHN_Korotkov/PDF_clear_version/detect_spike.jl")
@@ -23,9 +23,11 @@ Hs(x, k) = Statistics.mean(x) + k * Statistics.std(x)
 function load_vcat(sol, sol_times, len_charts)
     for index in 1:len_charts
         
+        println("index: $index"); flush(stdout)
         filename_sol = "$(index)_solution.mat"
         filename_sol_t = "$(index)_t_interval.mat"
 
+        println("open files"); flush(stdout)
         solve = matopen(folder*filename_sol)
         solve_t = matopen(folder*filename_sol_t)
         solve = read(solve, "solve")
@@ -35,18 +37,15 @@ function load_vcat(sol, sol_times, len_charts)
         if index == 1
             len_sol = length(solve_t)
             ttr = t_truncate(len_sol)
-            solve_x1 = solve[ttr:len_sol, 1]
+            solve = solve[ttr:len_sol]
             solve_t = solve_t[ttr:len_sol]
-        else
-            solve_x1 = solve[:, 1]
-            solve_t=  solve_t
         end
 
-        solve = nothing;
-        GC.gc();
-
-        sol = vcat(sol, solve_x1);
+        println("vcat"); flush(stdout)
+        sol = vcat(sol, solve);
         sol_times = vcat(sol_times, solve_t);
+
+        println("-----------------------"); flush(stdout)
     end
 
     return sol, sol_times
@@ -55,7 +54,8 @@ end
 folder = "/home/sergey/MEGA/matlab/files_long_time_series/"
 sol = Float64[]
 sol_times = Float64[]
-len_charts = 20
+
+len_charts = 10
 
 sol, sol_times = load_vcat(sol, sol_times, len_charts)
 
@@ -66,7 +66,6 @@ GC.gc();
 
 data_local_max = get_local_max(data)
 data_local_min = get_local_min(data)
-data = nothing;
 drop_artifacts(data_local_max, data_local_min)
 
 all_amplitudes = get_amplitudes_all_events(data_local_max[1], data_local_min[1])
@@ -87,9 +86,9 @@ array_PDF_IEI = get_PDF_IEI(array_IEI; shift = 20)
 
 plot_PDF_hist(array_IEI, array_PDF_IEI, 25)
 
-#= len_solve = length(solve_t)
+len_solve = length(data[2])
 tstart = len_solve - 500000; tend = len_solve
 f = Figure()
 ax = Axis(f[1, 1], xlabel = L"time", ylabel = L"x_1")
-lines!(ax, solve_t[tstart:tend], solve[tstart:tend, 1], linewidth = 1.0, color = :blue)
-display(GLMakie.Screen(), f) =#
+lines!(ax, data[2][tstart:tend], data[1][tstart:tend], linewidth = 1.0, color = :blue)
+display(GLMakie.Screen(), f) 
