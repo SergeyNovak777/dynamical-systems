a3a9ef7bd557cc782e8129f04cfeeb055d945cb8
Sergey Olenin <sergeyheart@mail.ru>
2024-06-18 15:14:17 +0300

    FHN map time 5000

---
+++ FHN_Korotkov/visualization/map_k1_k2_g_0_02_time_5000.jl
@@ -0,0 +1,47 @@
+if Sys.iswindows()
+    username = "Alex"
+    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
+    using Pkg
+    Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
+else
+    username = "sergey"
+    pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
+    using Pkg
+    Pkg.activate(pathtorepo * "/env/integrate/")
+end
+
+using JLD2, CairoMakie, GLMakie
+
+Λs = load("/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/data/maps_LSE/fix g = 0.02 change k2 and k1 time 5000/LSE_300x300_k_1_k_2.jld2")["λs"]
+
+length_range = 300;
+k1range = range( 0.001, 0.008, length = length_range);
+k2range = range(0.0, 1.0, length = length_range);
+
+index = 1
+absmax = maximum(abs.(Λs[:, :, index]))
+
+mn, mx =  -absmax, absmax
+CairoMakie.activate!()  
+f = Figure()
+ticksize = 35
+tickpad = 10.0
+textsize = 25
+textsizecurve = 30
+lw = 3.5
+mksize = 12
+
+ax = Axis(f[1, 1], xlabel = L"k_2",ylabel = L"k_1", xlabelsize = 50, ylabelsize = 50,
+            xticklabelsize = ticksize, yticklabelsize = ticksize,
+            xgridvisible  = false, ygridvisible = false,
+            xticklabelpad = tickpad, yticklabelpad = tickpad)
+
+hm = heatmap!(ax, k2range, k1range, transpose(Λs[:, :, index]), colormap = :seismic,
+                colorrange = (mn, mx))
+Colorbar(f[:, end+1], hm)
+display(GLMakie.Screen(), f);
+
+#=pathtosave = "/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/images/maps"
+filename = "/k1_k2_left_angle.eps"
+fullpath = pathtosave * filename 
+save(fullpath, f)=#
