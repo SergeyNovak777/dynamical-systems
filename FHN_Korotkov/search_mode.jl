34f755a258e1d354958a54f87088f7f1f8eb0341
Sergey_Olenin <sergeyheart@mail.ru>
2024-07-18 18:25:35 +0300

    FHN

--- FHN_Korotkov/search_mode.jl
+++ FHN_Korotkov/search_mode.jl
@@ -51,13 +51,14 @@ max_iters = 1e8;
 integrator_setting = get_set_integ_setting(alg, adaptive, abs_tol, rel_tol, max_iters);
 
 parameters = FHN2_try3_params();
+parameters[3]= 0.17
 parameters[7] =  0.09203007518796992
 parameters[8] = 64.76190476190476
 
 u0_start = [-1.0073393282360215, -0.6392350435710693, -1.022720802290101, -0.6276963254782901, -0.011538718093154163]
 u0_start = SVector{5}(u0_start);
 fixed_point = [-1.01, -0.6367552038435214, -1.01, -0.6367552038435204, -3.620121549323092e-13]
-t_end = 50_000;
+t_end = 10_000;
 tspan = (0.0, t_end);
 
 prob = ODEProblem(FHN2_try3, u0_start, tspan, parameters)
@@ -65,11 +66,11 @@ sol = solve(prob, integrator_setting.alg, adaptive = integrator_setting.adaptive
                 abstol = integrator_setting.abstol, reltol = integrator_setting.reltol, 
                 maxiters = integrator_setting.maxiters);
 
-ds = CoupledODEs(FHN2_try3, u0_start, parameters,
+#= ds = CoupledODEs(FHN2_try3, u0_start, parameters,
 diffeq = integrator_setting);
 
 LSE = lyapunovspectrum(ds, 10000);
-println(LSE);
+println(LSE); =#
 
 len_sol = length(sol.t);
 ttr = t_truncate(len_sol);
@@ -77,45 +78,39 @@ ttr = t_truncate(len_sol);
 labelsize = 85;
 ticksize = 50;
 t_plot_start = ttr;
-t_plot_end = t_plot_start + 50_000; #len_sol;
+t_plot_end = t_plot_start + 150_000;
 
-path_to_save = "/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/images/scenario/"
-
-CairoMakie.activate!();
 
 f = Figure(size = (1200 ,600));
-ax = Axis3(f[1, 1], xlabel = L"y_1", ylabel = L"y_2", zlabel = L"x_1",
+ax = Axis3(f[1, 1], xlabel = L"x_1", ylabel = L"x_2", zlabel = L"y_1",
     xlabelsize = labelsize, ylabelsize = labelsize, zlabelsize = labelsize,
     xticklabelsize = ticksize, yticklabelsize = ticksize, zticklabelsize = ticksize,
     xgridvisible = false, ygridvisible = false, zgridvisible = false,
-    xticks = [-0.63, -0.6], yticks = [-0.63, -0.6], zticks = [-1.07, -0.8],
     xlabeloffset = 60, ylabeloffset = 60, zlabeloffset = 115,
     protrusions = (30, 30,120, 30),
     elevation = 0.03pi);
-lines!(ax, sol[2, t_plot_start:t_plot_end], sol[4, t_plot_start:t_plot_end],
-        sol[1, t_plot_start:t_plot_end], linewidth = 1.5, color = :black);
-scatter!(ax, fixed_point[2], fixed_point[4], fixed_point[1], markersize = 15, color = :red)
-text!(ax, fixed_point[2], fixed_point[4], fixed_point[1], text = L"O_1", fontsize = labelsize, align = (:center, :top), offset = (0, -50))
+lines!(ax, sol[1, t_plot_start:t_plot_end], sol[3, t_plot_start:t_plot_end],
+        sol[2, t_plot_start:t_plot_end], linewidth = 1.5, color = :black);
 display(f);
 
-save(path_to_save * "narrowing.eps", f)
+#= ds = CoupledODEs(FHN2_try3, u0_start, parameters,
+diffeq = integrator_setting);
 
+LSE = lyapunovspectrum(ds, 10000);
+println(LSE);
 
-#= pmap = PoincareMap(ds, (1, -1.01))
-tr, trange = trajectory(pmap, 700000)
+pmap = PoincareMap(ds, (1, -1.01))
+tr, trange = trajectory(pmap, 200000)
 
 len_tr_map = length(trange);
 ttr_map = t_truncate(len_tr_map);
 
-t_plot_start_map = 500000;
+t_plot_start_map = 200000;
 t_plot_end_map = t_plot_start_map + 80_000;
 
 f = Figure(size = (900 ,900));
 ax = Axis(f[1, 1], xlabel = L"x_2", ylabel = L"y_2", xlabelsize = labelsize, ylabelsize = labelsize,
     xticklabelsize = ticksize, yticklabelsize = ticksize,
-    xgridvisible = false, ygridvisible = false,
-    xtickformat = "{:.1f}",
-    xticks = [-1.1, -1.0, -0.9, -0.81]);
-scatter!(ax, tr[t_plot_start_map:t_plot_end_map, 3], tr[t_plot_start_map:t_plot_end_map, 4], markersize = 2.0, color = :black);
-display(f);
-save(path_to_save * "narrowing_poincare.eps", f) =#
+    xgridvisible = false, ygridvisible = false);
+scatter!(ax, tr[t_plot_start_map:t_plot_end_map, 3], tr[t_plot_start_map:t_plot_end_map, 4], markersize = 1.0, color = :black);
+display(GLMakie.Screen(), f); =#
