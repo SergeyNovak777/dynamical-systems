e889a2e3583c98dc190c0f7701c9ec49dba93a50
SergeyNovak777 <sergeyheart@mail.ru>
2025-03-17 20:17:34 +0300

    add categorized map

--- FHN_Korotkov/visualization/maps_in_article/map_k1_[0.085_0.094]_k2_[60_80]_R3.jl
+++ FHN_Korotkov/visualization/maps_in_article/map_k1_[0.085_0.094]_k2_[60_80]_R3.jl
@@ -4,7 +4,7 @@ if Sys.iswindows()
     using Pkg
     Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
 else
-    username = "sergey"
+    username = "irrito"
     pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
     using Pkg
     Pkg.activate(pathtorepo * "/env/integrate/")
@@ -13,8 +13,8 @@ end
 using JLD2, CairoMakie, MAT, GLMakie
 
 
-Λs = load("/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/data/maps_LSE/R3/LSE_350x350_k_1_k_2.jld2")["λs"]
-u0s = load("/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/data/maps_LSE/R3/u0s_350x350_k_1_k_2.jld2")
+Λs = load("/home/irrito/MEGA downloads/dynamical-systems/FHN_Korotkov/data/maps_LSE/R3/LSE_350x350_k_1_k_2.jld2")["λs"]
+u0s = load("/home/irrito/MEGA downloads/dynamical-systems/FHN_Korotkov/data/maps_LSE/R3/u0s_350x350_k_1_k_2.jld2")
 init_point = u0s["init_points"]
 last_point = u0s["last_points"]
 
@@ -54,3 +54,39 @@ display(GLMakie.Screen(), f);
 filename = "/map_R3.pdf"
 fullpath = pathtosave * filename 
 save(fullpath, f) =#
+
+function check_condition(spectrum)
+    checknull(spectrum[1]) && spectrum[2]<0  ? color = 5 :
+    checknull(spectrum[1]) && checknull(spectrum[2])  && spectrum[3]<0 ? color = 4 :
+    checknull(spectrum[1]) && checknull(spectrum[2])  && checknull(spectrum[3]) && spectrum[4]<0 ? color = 3 :
+    spectrum[1]>0  && checknull(spectrum[2])  && spectrum[3]<0 ? color = 2 :
+    spectrum[1]>0  && spectrum[2]>0   && checknull(spectrum[3]) && spectrum[4]<0 ? color = 1 :
+    color = 6
+end
+
+function checknull(value)
+    isapprox(value, 0.0; atol = 1e-2)
+end
+
+color_matrix00 = zeros(350, 350)
+
+for (i, k1) in enumerate(k2range)
+    
+    for (j, k2) in enumerate(k1range)
+        spectrum = Λs[i, j, :]
+        color_matrix00[i, j] = check_condition(spectrum)
+    end
+end
+
+
+f = Figure()
+ax = Axis(f[1, 1], xlabel = L"k_2",ylabel = L"k_1", xlabelsize = 50, ylabelsize = 50,
+            xticklabelsize = ticksize, yticklabelsize = ticksize,
+            xgridvisible  = false, ygridvisible = false,
+            xticklabelpad = tickpad, yticklabelpad = tickpad)#,
+            #xticks = [63, 70, 79],
+            #yticks = [0.086, 0.090, 0.093])
+
+hm = heatmap!(ax, k2range, k1range, transpose(color_matrix00), colormap = :RdBu_6)
+Colorbar(f[:, end+1], hm)
+display(GLMakie.Screen(), f);
