8b27be32d09a2968c200380308d44dc12707e6f6
SergeyNovak777 <sergeyheart@mail.ru>
2025-03-24 20:20:16 +0300

    update route1

--- FHN_Korotkov/4d_system/figure_14.jl
+++ FHN_Korotkov/4d_system/figure_14.jl
@@ -1,37 +1,15 @@
-if Sys.iswindows()
-    username = "Alex"
-    pathtorepo = "C:\\Users\\" *username *  "\\Desktop\\"
-    using Pkg
-    Pkg.activate(pathtorepo * "dynamical-systems\\env\\integrate\\")
-else
-    username = "sergey"
-    pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
-    using Pkg
-    Pkg.activate(pathtorepo * "/env/integrate/")
-    include("/home/sergey/work/repo/dynamical-systems/system.jl")
-end
+username = "irrito"
+pathtorepo = "/home/" *username *"/work/repo/dynamical-systems"
+using Pkg
+Pkg.activate(pathtorepo * "/env/integrate/")
+include(pathtorepo * "/system.jl")
 
-using StaticArrays, DifferentialEquations, DynamicalSystems, CairoMakie, GLMakie
+
+using StaticArrays, SciMLBase, OrdinaryDiffEqVerner, CairoMakie, GLMakie
 
 function get_set_integ_setting(alg, adaptive, abs_tol, rel_tol, max_iters)
     integrator_setting = (alg = alg, adaptive = adaptive, abstol = abs_tol, reltol = rel_tol, maxiters = max_iters);
     return integrator_setting;
-end
-
-function FHN2_4d(u, p ,t)
-    x1, y1, x2, y2 = u
-    ϵ, a, g, k, σ, α, k1, k2 = p
-
-    I(ϕ_i) = g * (1.0/(1.0 + exp(k*(cos(σ/2) - cos(ϕ_i - α - σ/2)))))
-
-    ϕ2 = atan(y2, x2)
-    ϕ1 = atan(y1, x1)
-
-    dx1dt = (x1 - x1 ^ 3 / 3 - y1 + I(ϕ2) + (k1 + k2 * (y1 - y2)^2) * (x2 - x1) ) / ϵ
-    dy1dt = x1 - a
-    dx2dt = (x2 - x2 ^ 3 / 3 - y2 + I(ϕ1) + (k1 + k2 * (y1 - y2)^2) * (x1 - x2) ) / ϵ
-    dy2dt = x2 - a
-    return SVector(dx1dt, dy1dt, dx2dt, dy2dt)
 end
 
 t_truncate(t) = floor(Int64, t / 2)
@@ -48,7 +26,7 @@ parameters = FHN2_try3_params();
 parameters[3] = 0.1;
 parameters[7] = 0.09;
 parameters[8] = 75.74;
-filename_hist = "fig_14_f.png"
+filename_hist = "route1_h_k2=$(parameters[8]).eps"
 
 u0_start = [-0.9816946043747945, -0.6320919525134647, -1.0342265829731392, -0.638226338524071];
 u0_start = SVector{4}(u0_start);
@@ -61,27 +39,28 @@ sol = solve(prob, integrator_setting.alg, adaptive = integrator_setting.adaptive
                 abstol = integrator_setting.abstol, reltol = integrator_setting.reltol, 
                 maxiters = integrator_setting.maxiters);
 
-path_to_save = "/home/sergey/MEGA/dynamical-systems/FHN_Korotkov/images/rewrite_images/"
+path_to_save = "/home/irrito/MEGA2/dynamical_systems/FHN/images/"
 
-
-labelsize = 80;
-ticksize = 50;
+labelsize = 75;
+ticksize = 45;
 CairoMakie.activate!();
 length_sol = length(sol);
 ttr = t_truncate(length_sol)
 t_plot_start =  ttr;
-t_plot_end = t_plot_start +120_000; # 150_000
-                
+t_plot_end = t_plot_start +130_000; # 150_000
+xlimits = [-2, -1, 0, 1, 2] #[-1.1, -0.9, -0.75]
+zlimits = [-0.5, 0, 0.5] #[-0.63, -0.61, -0.59]
 indexx = 1; indexy  = 3; indexz = 4;
 f = Figure(size = (1000 ,600));
 ax = Axis3(f[1, 1], xlabel = L"x_1", ylabel = L"x_2", zlabel = L"y_2",
     xlabelsize = labelsize, ylabelsize = labelsize, zlabelsize = labelsize,
     xticklabelsize = ticksize, yticklabelsize = ticksize, zticklabelsize = ticksize,
     xgridvisible = false, ygridvisible = false, zgridvisible = false,
-    xlabeloffset = 60, ylabeloffset = 60, zlabeloffset = 85, protrusions = (20, 20,100, 20))
+    xlabeloffset = 60, ylabeloffset = 60, zlabeloffset = 150, protrusions = (20, 20,100, 20),
+    xticks = xlimits, yticks = xlimits, zticks = zlimits)
 
 lines!(ax, sol[indexx, t_plot_start:t_plot_end], sol[indexy, t_plot_start:t_plot_end],
-        sol[indexz, t_plot_start:t_plot_end], linewidth = 1.5, color = :black);
+        sol[indexz, t_plot_start:t_plot_end], linewidth = 0.7, color = :black);
 display(GLMakie.Screen(), f);
 
 save(path_to_save*filename_hist, f)
--- FHN_Korotkov/4d_system/route1 double resonance point/scenario_route1.jl
+++ FHN_Korotkov/4d_system/route1 double resonance point/scenario_route1.jl
@@ -5,19 +5,18 @@ Pkg.activate(pathtorepo * "/env/integrate/")
 include("/home/irrito/work/repo/dynamical-systems/system.jl")
 
 
-using StaticArrays, DifferentialEquations, DynamicalSystems
+using StaticArrays, SciMLBase, OrdinaryDiffEqVerner, ChaosTools
 using CairoMakie, GLMakie
 
 function get_set_integ_setting(alg, adaptive, abs_tol, rel_tol, max_iters)
     integrator_setting = (alg = alg, adaptive = adaptive, abstol = abs_tol, reltol = rel_tol, maxiters = max_iters);
     return integrator_setting;
 end
-
 t_truncate(t) = floor(Int64, t / 2)
 
 alg = Vern9()
-abs_tol = 1e-10;
-rel_tol = 1e-10;
+abs_tol = 1e-12;
+rel_tol = 1e-12;
 max_iters = 1e8;
 adaptive = true;
 
@@ -25,13 +24,12 @@ integrator_setting = get_set_integ_setting(alg, adaptive, abs_tol, rel_tol, max_
 
 parameters = FHN2_try3_params();
 parameters[7] =  0.09
-parameters[8] = 54.0
+parameters[8] = 75.74
 
-u0_start = sol[end]
-# [-1.0836728460611933, -0.6318417392022484, -0.9017528537331925, -0.624049721609583];
+u0_start = sol[end]#[-1.0836728460611933, -0.6318417392022484, -0.9017528537331925, -0.624049721609583];
 u0_start = SVector{4}(u0_start);
 
-t_end = 15_000;
+t_end = 2_000;
 tspan = (0.0, t_end);
 
 prob = ODEProblem(FHN2_4d, u0_start, tspan, parameters)
@@ -46,38 +44,55 @@ diffeq = integrator_setting);
 LSE = lyapunovspectrum(ds, 5000);
 println("LSE: $(LSE)");
 
-labelsize = 50;
-ticksize = 25;
+x1 = x2 = interval(-1.5, 1.5)
+y1 = y2 = interval(-1.5, 1.5)
+box = [x1, y1, x2, y2]
 
+fixed_point, eigs, _ = fixedpoints(ds, box)
+fixed_point = fixed_point[1];
+
+# Poincare Map
+pmap = PoincareMap(ds, (1, -1.015))
+tr, trange = trajectory(pmap, 300_000)
+
+labelsize = 70;
+ticksize = 35;
+markersize_fp = 12.0;
+markersize_torus = 3.0;
+len_tr_map = length(trange);
+ttr_map = t_truncate(len_tr_map);
+
+t_plot_start_map = ttr_map;
+t_plot_end_map = t_plot_start_map + 10_000
+f = Figure(size = (600 ,600));
+ax = Axis(f[1, 1], xlabel = L"x_2", ylabel = L"y_2", xlabelsize = labelsize, ylabelsize = labelsize,
+    xticklabelsize = ticksize, yticklabelsize = ticksize,
+    xgridvisible = false, ygridvisible = false)
+scatter!(ax, tr[t_plot_start_map:t_plot_end_map, 3], tr[t_plot_start_map:t_plot_end_map, 4],
+        markersize = markersize_torus, color = :black);
+#= scatter!(ax, fixed_point[3], fixed_point[4], markersize = markersize_fp, color = :red);
+text!(ax, fixed_point[3], fixed_point[4],
+        text = L"O_1", fontsize = 50, align = (:center, :bottom)) =#
+
+display(GLMakie.Screen(), f);
+
+filename = "route1_g_k2=$(parameters[8]).eps";
+path_to_save = "/home/irrito/MEGA2/dynamical_systems/FHN/images/route1/";
+
+save(path_to_save * filename, f)
+
+#= 
 length_sol = length(sol);
 ttr = t_truncate(length_sol)
 t_plot_start =  ttr
-t_plot_end = t_plot_start + 150_000
+t_plot_end = t_plot_start + 50_000
 
-indexx = 1; indexy  = 3; indexz = 2;
+indexx = 2; indexy  = 4; indexz = 1;
 f = Figure(size = (1200 ,600));
-ax = Axis3(f[1, 1], xlabel = L"x_1", ylabel = L"x_2", zlabel = L"y_1",
+ax = Axis3(f[1, 1], xlabel = L"y_1", ylabel = L"y_2", zlabel = L"x_1",
     xlabelsize = labelsize, ylabelsize = labelsize, zlabelsize = labelsize,
     xticklabelsize = ticksize, yticklabelsize = ticksize, zticklabelsize = ticksize,
     xgridvisible = false, ygridvisible = false, zgridvisible = false)#,
 lines!(ax, sol[indexx, t_plot_start:t_plot_end], sol[indexy, t_plot_start:t_plot_end],
         sol[indexz, t_plot_start:t_plot_end], linewidth = 1.5, color = :black);
-display(GLMakie.Screen(), f);
-
-
-# Poincare Map
-pmap = PoincareMap(ds, (1, -1.01))
-tr, trange = trajectory(pmap, 20000)
-
-len_tr_map = length(trange);
-ttr_map = t_truncate(len_tr_map);
-
-t_plot_start_map = ttr_map;
-t_plot_end_map = t_plot_start_map + 10_000;
-
-f = Figure(size = (900 ,900));
-ax = Axis(f[1, 1], xlabel = L"x_2", ylabel = L"y_2", xlabelsize = labelsize, ylabelsize = labelsize,
-    xticklabelsize = ticksize, yticklabelsize = ticksize,
-    xgridvisible = false, ygridvisible = false)
-scatter!(ax, tr[t_plot_start_map:t_plot_end_map, 3], tr[t_plot_start_map:t_plot_end_map, 4], markersize = 1.0, color = :black);
-display(GLMakie.Screen(), f);
+display(GLMakie.Screen(), f); =#
